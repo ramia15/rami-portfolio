2c73b1b560dd93a943198314ebf90499dcb4c8da
Rami <ramiabdel15@gmail.com>
2023-09-12 01:41:17 -0500

    possible fix for email

--- api/send-email.js
+++ api/send-email.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const fetch = require('node-fetch');
+const formidable = require('formidable');
 
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
@@ -7,49 +8,58 @@ module.exports = async (req, res) => {
     }
 
     try {
-        const { name, email, message, 'g-recaptcha-response': recaptchaResponse } = req.body;
+        const form = new formidable.IncomingForm();
 
-        // Check for required fields
-        if (!name || !email || !message || !recaptchaResponse) {
-            console.error('Missing required fields.');
-            return res.status(400).send({ success: false, message: 'Missing required fields.' });
-        }
-
-        // Verify reCAPTCHA
-        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`;
-        const recaptchaVerification = await fetch(verificationURL, { method: 'POST' });
-        const recaptchaResult = await recaptchaVerification.json();
-
-        if (!recaptchaResult.success) {
-            console.error('reCAPTCHA verification failed.');
-            return res.status(400).json({ success: false, message: 'reCAPTCHA verification failed.' });
-        }
-
-        // Configure nodemailer
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.YOUR_EMAIL_ADDRESS,
-                pass: process.env.YOUR_EMAIL_PASSWORD
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                console.error('Error parsing form:', err);
+                return res.status(500).send({ success: false, message: 'Failed to parse form.' });
             }
-        });
 
-        // Email options
-        let mailOptions = {
-            from: email,
-            to: 'ramiabdel15@gmail.com',
-            subject: 'New form submission',
-            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-        };
+            const { name, email, message, 'g-recaptcha-response': recaptchaResponse } = fields;
 
-        // Send email
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Email sending error:', error.message);
-                return res.status(500).send({ success: false, message: 'Failed to send email.' });
+            // Check for required fields
+            if (!name || !email || !message || !recaptchaResponse) {
+                console.error('Missing required fields.');
+                return res.status(400).send({ success: false, message: 'Missing required fields.' });
             }
-            console.log('Email sent successfully:', info.response);
-            res.status(200).send({ success: true, info: info.response });
+
+            // Verify reCAPTCHA
+            const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`;
+            const recaptchaVerification = await fetch(verificationURL, { method: 'POST' });
+            const recaptchaResult = await recaptchaVerification.json();
+
+            if (!recaptchaResult.success) {
+                console.error('reCAPTCHA verification failed.');
+                return res.status(400).json({ success: false, message: 'reCAPTCHA verification failed.' });
+            }
+
+            // Configure nodemailer
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.YOUR_EMAIL_ADDRESS,
+                    pass: process.env.YOUR_EMAIL_PASSWORD
+                }
+            });
+
+            // Email options
+            let mailOptions = {
+                from: email,
+                to: 'ramiabdel15@gmail.com',
+                subject: 'New form submission',
+                text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+            };
+
+            // Send email
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error('Email sending error:', error.message);
+                    return res.status(500).send({ success: false, message: 'Failed to send email.' });
+                }
+                console.log('Email sent successfully:', info.response);
+                res.status(200).send({ success: true, info: info.response });
+            });
         });
 
     } catch (error) {
