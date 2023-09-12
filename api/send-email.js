84de83e3b77a56fefe7ddcb8cbcfbcc75f69c2fb
Rami <ramiabdel15@gmail.com>
2023-09-12 00:07:38 -0500

    fix form

--- api/send-email.js
+++ api/send-email.js
@@ -1,18 +1,21 @@
-
 const nodemailer = require('nodemailer');
 const fetch = require('node-fetch');
 
-const THE_RECAPTCHA_SECRET_KEY = 'RECAPTCHA_SECRET_KEY';
-
 module.exports = async (req, res) => {
-    if (req.method === 'POST') {
+    if (req.method !== 'POST') {
+        return res.status(405).send({ error: 'Only POST requests are allowed' });
+    }
+
+    // Check for required fields
+    const { name, email, message, 'g-recaptcha-response': recaptchaResponse } = req.body;
+    if (!name || !email || !message || !recaptchaResponse) {
+        return res.status(400).send({ success: false, message: 'Missing required fields.' });
+    }
+
+    try {
         // Verify reCAPTCHA
-        const recaptchaResponse = req.body['g-recaptcha-response'];
-        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`;
-        
-        const recaptchaVerification = await fetch(verificationURL, {
-            method: 'POST',
-        });
+        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaResponse}`;
+        const recaptchaVerification = await fetch(verificationURL, { method: 'POST' });
         const recaptchaResult = await recaptchaVerification.json();
 
         if (!recaptchaResult.success) {
@@ -23,13 +26,10 @@ module.exports = async (req, res) => {
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'YOUR_EMAIL_ADDRESS',
-                pass: 'YOUR_EMAIL_PASSWORD'
+                user: process.env.YOUR_EMAIL_ADDRESS,
+                pass: process.env.YOUR_EMAIL_PASSWORD
             }
         });
-
-        // Extract form data
-        const { name, email, message } = req.body;
 
         // Email options
         let mailOptions = {
@@ -42,12 +42,14 @@ module.exports = async (req, res) => {
         // Send email
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
-                res.status(500).send({ success: false, error: error.message });
-            } else {
-                res.status(200).send({ success: true, info: info.response });
+                console.error('Email sending error:', error.message); // Log detailed error server-side
+                return res.status(500).send({ success: false, message: 'Failed to send email.' });
             }
+            res.status(200).send({ success: true, info: info.response });
         });
-    } else {
-        res.status(405).send({ error: 'Only POST requests are allowed' });
+
+    } catch (error) {
+        console.error('Unexpected error:', error.message); // Log detailed error server-side
+        return res.status(500).send({ success: false, message: 'An unexpected error occurred.' });
     }
 };
