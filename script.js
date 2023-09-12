87623b2a14e974e5358e4b4d0c97dcafdc846849
Rami <ramiabdel15@gmail.com>
2023-09-12 10:58:20 -0500

    fixed layout

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Register ScrollTrigger plugin
-    gsap.registerPlugin(ScrollTrigger);
-
-    // Stagger fade in the projects with cascade effect from right
-    gsap.utils.toArray('.project').forEach((project, index) => {
-        gsap.fromTo(project, 
-            { x: '100%', opacity: 0 }, // From state
-            {
-                x: '0%', // To state
-                opacity: 1,
-                delay: index * 0.2,
-                scrollTrigger: {
-                    trigger: project,
-                    start: 'top bottom-=100', // Adjusting this to start the animation a bit sooner
-                    end: 'bottom top',
-                    toggleActions: 'play none none reverse'
-                }
-            });
-    });
-
     // Filter content function
     const filterButtons = document.querySelectorAll('.btn-filter');
     filterButtons.forEach(button => {
         button.addEventListener('click', (e) => {
             const type = e.target.getAttribute('data-type');
-            
-            // Pause any active GSAP animations
-            gsap.globalTimeline.pause();
-            
             filterContent(type);
-            
-            // Refresh ScrollTrigger after the animations
-            ScrollTrigger.refresh();
         });
     });
 
@@ -42,30 +15,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const webDevBtn = document.querySelector('.btn-filter[data-type="web-dev"]');
         const graphicDesignBtn = document.querySelector('.btn-filter[data-type="graphic-design"]');
 
-        // This function will animate the projects into view
-        function animateProjects(container) {
-            gsap.utils.toArray(container.children).forEach((project, index) => {
-                gsap.from(project, {
-                    x: '100%',
-                    y: 100,
-                    opacity: 0,
-                    delay: index * 0.2
-                });
-            });
-        }
-
         if (type === 'web-dev') {
             webDevSection.style.display = 'block';
             graphicDesignSection.style.display = 'none';
             webDevBtn.classList.add('active');
             graphicDesignBtn.classList.remove('active');
-            animateProjects(webDevSection); // Animate the web-dev projects
         } else if (type === 'graphic-design') {
             graphicDesignSection.style.display = 'block';
             webDevSection.style.display = 'none';
             graphicDesignBtn.classList.add('active');
             webDevBtn.classList.remove('active');
-            animateProjects(graphicDesignSection); // Animate the graphic-design projects
         }
     }
 
---
+++ test.js
@@ -0,0 +1,189 @@
+
+const background = document.querySelector('.background');
+gsap.set(background, {
+    position: 'fixed',
+    width: '100vw',
+    height: '100vh',
+    top: 0,
+    left: 0,
+    background: '#141414',
+    overflow: 'hidden',
+    zIndex: -100
+});
+
+const spans = document.querySelectorAll('.background span');
+gsap.set(spans, {
+    width: '50vmin',
+    height: '50vmin',
+    borderRadius: '50vmin',
+    backfaceVisibility: 'hidden',
+    position: 'absolute'
+});
+
+
+gsap.set(spans[0], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[0], {
+    rotation: 360,
+    transformOrigin: '-22vw -16vh',
+    boxShadow: '100vmin 0 13.414128397628808vmin currentColor',
+    duration: 193,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '84%',
+    left: '38%'
+});
+    
+gsap.set(spans[1], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[1], {
+    rotation: 360,
+    transformOrigin: '-7vw 13vh',
+    boxShadow: '-100vmin 0 13.285120293367132vmin currentColor',
+    duration: 157,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '8%',
+    left: '87%'
+});
+    
+gsap.set(spans[2], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[2], {
+    rotation: 360,
+    transformOrigin: '5vw -21vh',
+    boxShadow: '100vmin 0 12.933457838566937vmin currentColor',
+    duration: 153,
+    repeat: -1,
+    color: '#583C87',
+    top: '76%',
+    left: '49%'
+});
+    
+gsap.set(spans[3], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[3], {
+    rotation: 360,
+    transformOrigin: '19vw 23vh',
+    boxShadow: '-100vmin 0 13.432373972452893vmin currentColor',
+    duration: 176,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '92%',
+    left: '51%'
+});
+    
+gsap.set(spans[4], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[4], {
+    rotation: 360,
+    transformOrigin: '-20vw -12vh',
+    boxShadow: '-100vmin 0 13.000772814922291vmin currentColor',
+    duration: 55,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '92%',
+    left: '61%'
+});
+    
+gsap.set(spans[5], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[5], {
+    rotation: 360,
+    transformOrigin: '-14vw 10vh',
+    boxShadow: '-100vmin 0 12.608171893618925vmin currentColor',
+    duration: 165,
+    repeat: -1,
+    color: '#583C87',
+    top: '57%',
+    left: '65%'
+});
+    
+gsap.set(spans[6], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[6], {
+    rotation: 360,
+    transformOrigin: '21vw -22vh',
+    boxShadow: '-100vmin 0 13.274572983984964vmin currentColor',
+    duration: 105,
+    repeat: -1,
+    color: '#583C87',
+    top: '16%',
+    left: '74%'
+});
+    
+gsap.set(spans[7], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[7], {
+    rotation: 360,
+    transformOrigin: '-10vw -7vh',
+    boxShadow: '100vmin 0 12.793673199870744vmin currentColor',
+    duration: 6,
+    repeat: -1,
+    color: '#583C87',
+    top: '92%',
+    left: '40%'
+});
+    
+gsap.set(spans[8], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[8], {
+    rotation: 360,
+    transformOrigin: '-22vw -12vh',
+    boxShadow: '100vmin 0 12.639027962072472vmin currentColor',
+    duration: 289,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '22%',
+    left: '6%'
+});
+    
+gsap.set(spans[9], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[9], {
+    rotation: 360,
+    transformOrigin: '-6vw 13vh',
+    boxShadow: '100vmin 0 13.450434246512222vmin currentColor',
+    duration: 212,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '91%',
+    left: '62%'
+});
+    
+gsap.set(spans[10], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[10], {
+    rotation: 360,
+    transformOrigin: '24vw -12vh',
+    boxShadow: '100vmin 0 13.093785681558426vmin currentColor',
+    duration: 298,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '25%',
+    left: '27%'
+});
+    
+gsap.set(spans[11], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[11], {
+    rotation: 360,
+    transformOrigin: '-20vw -12vh',
+    boxShadow: '-100vmin 0 13.000772814922291vmin currentColor',
+    duration: 55,
+    repeat: -1,
+    color: '#6cc0cb',
+    top: '92%',
+    left: '61%'
+});
+    
+gsap.set(spans[12], {transform: 'translate3d(0, 0, 0)'}); 
+gsap.to(spans[12], {
+    rotation: 360,
+    transformOrigin: '-14vw 10vh',
+    boxShadow: '-100vmin 0 12.608171893618925vmin currentColor',
+    duration: 165,
+    repeat: -1,
+    color: '#583C87',
+    top: '57%',
+    left: '65%'
+});
+    
+document.addEventListener('mousemove', (e) => {
+    const x = (window.innerWidth / 2 - e.pageX) / 20;
+    const y = (window.innerHeight / 2 - e.pageY) / 20;
+    gsap.to(spans, {
+        x: x,
+        y: y,
+        duration: 0.5,
+        overwrite: 'auto'
+    });
+});
