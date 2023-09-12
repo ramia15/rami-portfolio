f2994ffa7020007e7187346211af188a907b1595
Rami <ramiabdel15@gmail.com>
2023-09-12 00:59:18 -0500

    fixing errors

--- toast.js
+++ toast.js
@@ -1,8 +1,9 @@
-document.getElementById('contact').addEventListener('submit', async function(event) {
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const toastElement = document.getElementById('toast');
     const toastMessageElement = document.getElementById('toastMessage');
+    const toastIconElement = document.getElementById('toastIcon');
 
     try {
         const response = await fetch('/api/send-email', {
@@ -14,13 +15,16 @@ document.getElementById('contact').addEventListener('submit', async function(eve
 
         if (result.success) {
             toastMessageElement.textContent = 'Email sent successfully!';
+            toastIconElement.innerHTML = '<i class="fas fa-check-circle"></i>';
             toastElement.style.backgroundColor = '#4CAF50';
         } else {
             toastMessageElement.textContent = 'Failed to send email. Please try again later.';
+            toastIconElement.innerHTML = '<i class="fas fa-exclamation-triangle"></i>';
             toastElement.style.backgroundColor = '#f44336';
         }
     } catch (error) {
         toastMessageElement.textContent = 'An error occurred. Please try again later.';
+        toastIconElement.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
         toastElement.style.backgroundColor = '#f44336';
     }
 
